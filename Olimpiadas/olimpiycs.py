c300a03265649ba2ffa2ea32c7f5a1a55452997b
Andresrs1014 <andresquinteroarcila@gmail.com>
2025-06-21 10:46:40 -0500

    first commit

---
+++ Olimpiadas/olimpiycs.py
@@ -0,0 +1,56 @@
+ 
+
+class Olimpycs:
+    
+    def __init__(self):
+        pass
+    
+    
+    def register_event(self):
+        pass
+       
+
+
+
+
+ 
+ 
+ 
+ 
+print('\n Simulador de Juegos Olimpicos ')
+
+ 
+while True:
+    
+    print()
+ 
+    print('1. Registro de eventos.')
+    print('2. Registro de participantes.')
+    print('3. Simulación de eventos.')
+    print('4. Creación de informes.')
+    print('5. Salir del programa.')
+    
+    option = input('Elige una opcion: ')
+    
+    
+    match option:
+        case '1':
+            pass
+        case '2':
+            pass
+        case '3':
+            pass
+        case '4':
+            pass
+        case '5':
+            print('Saliendo del simulador..')
+            break
+        case _:
+            print('Opcion invalida, por favor elige una nueva')    
+    
+
+  
+  
+
+ 
+ 
